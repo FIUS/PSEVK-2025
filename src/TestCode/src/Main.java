a0cfe0d807c38bca43878f819e456de369ce72ba
Linus Harder <weesulinus@gmx.net>
2025-07-31 13:28:17 +0200

    random num tester wip

--- src/TestCode/src/Main.java
+++ src/TestCode/src/Main.java
@@ -1,11 +1,13 @@
+import javax.swing.plaf.IconUIResource;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
+public class Main {;
     static int evalCycles = 100000;
     static int seedBound = 100000;
     static long resultBound = 10000000000L;
+    static int resultDigits = 10;
     static double offsetTolerance = 0.1;
 
     public static void main(String[] args) {
@@ -27,13 +29,47 @@ public class Main {
     public static void eval(ArrayList<Long> results) {
         long start = System.nanoTime();
 
-        String resultString = results.stream()
-                .map(num -> String.format("%010d", num))
-                .collect(Collectors.joining());
+        numberOccurenceAnalysis(results);
+        System.out.println((System.nanoTime() - start) / 10000000);
+    }
 
-        Map<Character, Long> countMap = resultString.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    public static void levenshteinDistanceAnalysis(ArrayList<Long> results) {
+        String curString = "";
+        String prevString = results.getFirst().toString();
+        long totalDistance = 0;
+
+        for (int i = 1; i < results.size(); i++) {
+            curString = addLeadingZeros(results.get(i), resultDigits);
+
+            for (int j = 0; j < curString.length(); j++) {
+                if (curString.charAt(j) != prevString.charAt(j)) {
+                    totalDistance++;
+                }
+            }
+            prevString = curString;
+        }
+
+        
+    }
+
+    public static String addLeadingZeros(long number, int numDigits) {
+        return String.format("%0"+numDigits+"d", number);
+    }
+
+    public static void numberOccurenceAnalysis (ArrayList<Long> results) {
+        Map<Character, Integer> countMap = new HashMap<>();
+
+        for (Integer i = 0; i < 10; i++) {
+            countMap.put(i.toString().charAt(0), 0);
+        }
+
+        for (Long result: results){
+            String resultString = addLeadingZeros(result, resultDigits);
+            for (char c: resultString.toCharArray()) {
+                countMap.put(c, countMap.get(c) + 1);
+            }
+        }
+
 
         System.out.println("Die Ziffernverteilung von deinem Zufallsgenerator");
         DecimalFormat df = new DecimalFormat("###.#");
@@ -41,9 +77,12 @@ public class Main {
         countMap.forEach((k, v) -> System.out.println(k + " : " + df.format((double) v / seedBound * 10) + " % = " + v));
 
 
-        for (long v : countMap.values()) {
-            if (v > seedBound * (1 + offsetTolerance)) {
-                System.out.println("zu viele " + v + "'s");
+        for (Character number : countMap.keySet()) {
+            if (countMap.get(number) > seedBound * (1 + offsetTolerance)) {
+                System.out.println("Es sind tendenziell zu viele " + number + " in deinen Zufallszahlen Vorhanden.");
+            }
+            if (countMap.get(number) < seedBound * (1 - offsetTolerance)) {
+                System.out.println("Es sind tendenziell zu wenige " + number + " in deinen Zufallszahlen Vorhanden.");
             }
         }
     }
