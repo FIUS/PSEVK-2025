f9a22f571de95463e3c8279d0d02a97eeee80e10
Linus Harder <weesulinus@gmx.net>
2025-08-02 23:08:17 +0200

    random aufgabe analyseprogramm levenshteinanalyse

--- src/TestCode/src/Main.java
+++ src/TestCode/src/Main.java
@@ -4,11 +4,12 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {;
-    static int evalCycles = 100000;
+    static int evalCycles = 1000000;
     static int seedBound = 100000;
-    static long resultBound = 10000000000L;
     static int resultDigits = 10;
+    static long resultBound = (long) Math.pow(10, resultDigits);
     static double offsetTolerance = 0.1;
+    static double levenshteinTolerance = 0.05;
 
     public static void main(String[] args) {
         Random rnd = new Random();
@@ -23,33 +24,72 @@ public class Main {;
     }
 
     public static long randomize(long seed) {
-        return seed * 100000 + seed;
-    }
-
-    public static void eval(ArrayList<Long> results) {
-        long start = System.nanoTime();
-
-        numberOccurenceAnalysis(results);
-        System.out.println((System.nanoTime() - start) / 10000000);
+        return seed * 100000;// + seed;
     }
 
     public static void levenshteinDistanceAnalysis(ArrayList<Long> results) {
         String curString = "";
-        String prevString = results.getFirst().toString();
+        String prevString = addLeadingZeros(results.getFirst(), resultDigits);
         long totalDistance = 0;
+        Map<Integer, Long> allDistances = new HashMap<>();
+        for (Integer i = 0; i < resultDigits; i++) {
+            allDistances.put(i, 0L);
+        }
 
         for (int i = 1; i < results.size(); i++) {
             curString = addLeadingZeros(results.get(i), resultDigits);
 
             for (int j = 0; j < curString.length(); j++) {
-                if (curString.charAt(j) != prevString.charAt(j)) {
-                    totalDistance++;
+                try {
+                    if (curString.charAt(j) != prevString.charAt(j)) {
+                        totalDistance++;
+                        allDistances.put(j, allDistances.get(j) + 1);
+                    }
+                } catch (Exception e) {
+                    System.out.println(curString);
+                    System.out.println(prevString);
+                    throw e;
                 }
             }
             prevString = curString;
         }
 
-        
+        double avgDistance = (double) totalDistance / (results.size() - 1);
+
+        long expectedDist = Math.round(Math.log10(resultBound) - 1);
+
+        DecimalFormat df = new DecimalFormat("#.###");
+        System.out.println("Die Durchschnittliche Levenshtein Distanz zwischen deinen generierten Zufallszahlen ist: " + df.format(avgDistance));
+        System.out.println("Der Erwartungswert hierfür ist: " + expectedDist);
+        if (avgDistance > expectedDist + expectedDist * levenshteinTolerance || avgDistance < expectedDist - expectedDist * levenshteinTolerance ){
+            System.out.println("Versuch näher an den Erwartungswert ranzukommen.");
+        }
+
+        System.out.println("Die Levenshtein Distanz, nach den Stellen in der Ergebniszahl aufgeschlüsselt sieht so aus:");
+        System.out.println("Der Erwartungswert für jede einzelne Stelle ist 0,9");
+
+        allDistances.forEach((k, v) -> System.out.println(k + " : " + df.format((double) v / evalCycles)));
+
+        for (Integer position : allDistances.keySet()){
+            if ((double) allDistances.get(position) / evalCycles < (double) expectedDist / 10 * (1 - levenshteinTolerance)) {
+                System.out.println("Es ist tendenziell zu wenig Variation an der " + position + ". Stelle deiner Zufallszahlen Vorhanden.");
+            }
+            if ((double) allDistances.get(position) / evalCycles > (double) expectedDist / 10 * (1 + levenshteinTolerance)) {
+                System.out.println("Es ist tendenziell zu viel Variation an der " + position + ". Stelle deiner Zufallszahlen Vorhanden.");
+            }
+        }
+    }
+
+    public static void eval(ArrayList<Long> results) {
+        long start = System.nanoTime();
+
+        System.out.println("-------------------------------");
+        System.out.println("Ziffernanalyse:");
+        numberOccurenceAnalysis(results);
+        System.out.println("-------------------------------");
+        System.out.println("Levenshteinanalyse:");
+        levenshteinDistanceAnalysis(results);
+        System.out.println((System.nanoTime() - start) / 10000000);
     }
 
     public static String addLeadingZeros(long number, int numDigits) {
@@ -74,14 +114,14 @@ public class Main {;
         System.out.println("Die Ziffernverteilung von deinem Zufallsgenerator");
         DecimalFormat df = new DecimalFormat("###.#");
 
-        countMap.forEach((k, v) -> System.out.println(k + " : " + df.format((double) v / seedBound * 10) + " % = " + v));
+        countMap.forEach((k, v) -> System.out.println(k + " : " + df.format((double) v / evalCycles * 10) + " % = " + v));
 
 
         for (Character number : countMap.keySet()) {
-            if (countMap.get(number) > seedBound * (1 + offsetTolerance)) {
+            if (countMap.get(number) > evalCycles * (1 + offsetTolerance)) {
                 System.out.println("Es sind tendenziell zu viele " + number + " in deinen Zufallszahlen Vorhanden.");
             }
-            if (countMap.get(number) < seedBound * (1 - offsetTolerance)) {
+            if (countMap.get(number) < evalCycles * (1 - offsetTolerance)) {
                 System.out.println("Es sind tendenziell zu wenige " + number + " in deinen Zufallszahlen Vorhanden.");
             }
         }
