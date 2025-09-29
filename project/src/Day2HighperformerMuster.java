42ed59c34daff9013a1adece3bfdc9599c8cda69
Linus Harder <weesulinus@gmx.net>
2025-09-29 16:24:02 +0200

    tag2 muster vereinfacht

--- project/src/Day2HighperformerMuster.java
+++ project/src/Day2HighperformerMuster.java
@@ -1,7 +1,9 @@
 public class Day2HighperformerMuster {
     public static void main(String[] args) {
-        int numVars = 7;
-        int[] vals = new int[numVars];
+        int numVars = 8;
+
+        int wahr = 0;
+        int falsch = 0;
 
         boolean erfuellbar = false;
         boolean tautologie = true;
@@ -11,33 +13,35 @@ public class Day2HighperformerMuster {
             String binary = Integer.toBinaryString(i);
             System.out.println(binary);
 
-            for (int j = 0; j < numVars; j++) {
-                if (binary.length() <= j) {
-                    vals[j] = 0;
-                } else {
-                    vals[j] = binary.charAt(j) - '0';
-                }
+            while (binary.length() < numVars) {
+                binary = "0" + binary;
             }
 
-            boolean a = vals[0] == 1;
-            boolean b = vals[1] == 1;
-            boolean c = vals[2] == 1;
-            boolean d = vals[3] == 1;
-            boolean e = vals[4] == 1;
-            boolean f = vals[5] == 1;
-            boolean g = vals[6] == 1;
+            boolean a = binary.charAt(0) == '1';
+            boolean b = binary.charAt(1) == '1';
+            boolean c = binary.charAt(2) == '1';
+            boolean d = binary.charAt(3) == '1';
+            boolean e = binary.charAt(4) == '1';
+            boolean f = binary.charAt(5) == '1';
+            boolean g = binary.charAt(6) == '1';
+            boolean h = binary.charAt(7) == '1';
 
-            if ((!(a && b) || a) && (!(c && d) || c) && (e || !e)) {
+            if (((a && b && !c && !d && !e) || (!a && c && d && !b && !e) || (!b && !c
+                    && !d && e && f)) && (!g || h)) {
+                wahr++;
                 erfuellbar = true;
                 unerfuellbar = false;
             } else {
+                falsch++;
                 tautologie = false;
             }
         }
 
-        System.out.println(erfuellbar);
-        System.out.println(unerfuellbar);
-        System.out.println(tautologie);
+        System.out.println("Die Formel ist erfüllbar ist: " + erfuellbar);
+        System.out.println("Die Formel ist unerfüllbar ist: " + unerfuellbar);
+        System.out.println("Die Formel ist eine Tautologie ist: " + tautologie);
+        System.out.println("Die Formel wird bei " + wahr + " von " + (int) Math.pow(2, numVars) + " Belegungen wahr");
+        System.out.println("Die Wahrscheinlichkeit das die Formel bei einer zufälligen Belegung wahr wird ist " + wahr / Math.pow(2, numVars) * 100 + "%");
 
     }
 }
